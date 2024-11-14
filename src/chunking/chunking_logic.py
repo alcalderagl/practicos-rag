f09f7aeba169af8aab356a862a683ca8166cfcfb
alcalderagl <145729419+alcalderagl@users.noreply.github.com>
2024-11-13 22:37:55 -0600

    :style:(checkstyle): chuncking

--- src/chunking/chunking_logic.py
+++ src/chunking/chunking_logic.py
@@ -1,25 +1,24 @@
 # nl library
 import spacy
+
 # chuncking with langchaing library
 from langchain.text_splitter import CharacterTextSplitter
 
-def chuck_doc(document:str):
+
+def chuck_doc(document: str):
     text_splitter = CharacterTextSplitter(
-        chunk_size = 35,
-        chunk_overlap=0,
-        separator='',
-        strip_whitespace=True
+        chunk_size=35, chunk_overlap=0, separator="", strip_whitespace=True
     )
     # chucking docs
     docs = text_splitter.create_documents([document])
-    # return a dict array with the chuncks
-    document_dicts = [{"metadata": doc.metadata, "page_content": doc.page_content} for doc in docs]
+    # return a dict array with the chuncks
+    document_dicts = (
+        {"metadata": doc.metadata, "page_content": doc.page_content} for doc in docs
+    )
     return document_dicts
 
 
-
-
-def clean_doc(document:str):
+def clean_doc(document: str):
     # if you have more computational ressources use es_core_news_lg or es_dep_news_trf
     nlp = spacy.load("es_core_news_sm")
     """
@@ -33,7 +32,11 @@ def clean_doc(document:str):
     # tokenization of document
     doc = nlp.tokenizer(document)
     # cleaning text
-    doc = [token.text.lower() for token in doc if not token.is_stop and token.is_alpha and len(token.text) > 2]
+    doc = (
+        token.text.lower()
+        for token in doc
+        if not token.is_stop and token.is_alpha and len(token.text) > 2
+    )
     # join the tokens into one string
     doc = " ".join(doc)
     # returning the doc preprocessed
