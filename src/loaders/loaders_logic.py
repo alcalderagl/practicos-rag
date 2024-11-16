288eacf4114db72694831e25e1ee4f5cc4a3f630
alcalderagl <145729419+alcalderagl@users.noreply.github.com>
2024-11-16 00:12:34 -0600

    :feat:(loaders):load pdf files logic

--- src/loaders/loaders_logic.py
+++ src/loaders/loaders_logic.py
@@ -1,59 +1,67 @@
-from langchain_community.document_loaders import PyPDFLoader, OnlinePDFLoader
+from src.commons.logging_messages import LOGG_MESSAGES
+from .pdf_loader_logic import PDFLoader
+from src.commons.models.response_logic import ResponseLogic
+from src.commons.enums.type_message import TypeMessage
+from src.commons.files_logic import (
+    createFolder,
+    existsFile,
+    save_document,
+    generate_file_path,
+    file_extension,
+)
+
+# ['pdf', 'csv', 'xlsx', 'xls', 'docx', 'txt']
 
 
-def load_pfd(path: str, islocal: bool = True):
-    """
-    this function could load online or local pdf
-    @path: it could be an URL if it's not loca and a path if it is local
-    @isLocal: it determines if it's local o remote load
-    returns a generator object to yields one item at time (lazy-evaluated)
-    """
-
-    try:
-        # catch an error in a friendly way to dont stop program execution
-        if islocal:
-            # if it is local then
-            _load_local_pdf(path)
-        else:
-            # otherwise
-            _load_online_pdf(path)
-    except Exception as e:
-        # catching the exception error
-        print(f"Error while loading PDF: {e}")
-        return []
+def upload_document(dir: str, document: any, filename: str):
+    createFolder(dir)
+    file_path = generate_file_path(dir, filename)
+    file_already_exist = existsFile(file_path)
+    if file_already_exist:
+        message = LOGG_MESSAGES["LOADER_FILE_ALREADY_EXIST"].format(filename=filename)
+        response = ResponseLogic(
+            message=message, typeMessage=TypeMessage.WARNING, response=[]
+        )
+        return response
+    else:
+        save_document(file_path, document)
+        response = _load_document(filename, file_path)
+        return response
 
 
-def _load_local_pdf(path_route: str):
-    """
-    this function loads a local pdf by path_route
-    returns an object structured to be processed by chuncking
-    """
-    loader = PyPDFLoader(path_route)
-    data = loader.load_and_split()
-    return _builderObject(data)
+def _load_document(filename: str, file_path: str):
+    file_ext = file_extension(filename)
+    documents = _document_loaders()
+    if file_ext in documents:
+        try:
+            loader_response = documents[file_ext](file_path)
+            result_loader = {"file_ext": file_ext, "response": loader_response}
+            message = LOGG_MESSAGES["LOADER_DOCUMENT_ADDED"].format(filename=filename)
+            response = ResponseLogic(
+                message=message, response=result_loader, typeMessage=TypeMessage.INFO
+            )
+            return response
+        except Exception as e:
+            # catching the exception error
+            message = LOGG_MESSAGES["LOADER_ERROR_LOADING"].format(error=e)
+            response = ResponseLogic(
+                message=message, typeMessage=TypeMessage.ERROR, response=[]
+            )
+            return response
+    else:
+        message = LOGG_MESSAGES["LOADER_UNKNOWN_FILE"].format(
+            filename=filename, ext=file_ext
+        )
+        response = ResponseLogic(
+            message=message, typeMessage=TypeMessage.WARNING, response=[]
+        )
+        return response
 
 
-def _load_online_pdf(pdf_URL: str):
-    """
-    this function loads an online pdf by webURL
-    returns an object structured to be processed by chuncking
-    """
-    loader = OnlinePDFLoader(pdf_URL)
-    data = loader.load_and_split()
-    return _builderObject(data)
-
-
-def _builderObject(data):
-    """
-    this returns a generator object (lazy-evaluated)
-    """
-    pages = (
-        {"page_content": page.page_content, "source": page.metadata["source"]}
-        for page in data
-    )
-    return pages
-
-
-# if __name__ == '__main__':
-#     test_it = "https://aragohv.com/wp-content/uploads/2019/06/comportamientofelino.pdf"
-#     load_pfd(test_it, False)
+def _document_loaders():
+    documents = {
+        #'csv': lambda path: (),
+        "pdf": lambda path: PDFLoader(path)
+        #'txt': lambda path: (),
+    }
+    return documents
