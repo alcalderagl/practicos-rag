cc3020be52ad4276e888b30b662ae2f8124e9baf
alcalderagl <145729419+alcalderagl@users.noreply.github.com>
2024-11-16 17:41:34 -0600

    :feat:(clean-doc): fixing cleaning

--- src/chunking/cleaning_doc_logic.py
+++ src/chunking/cleaning_doc_logic.py
@@ -3,8 +3,6 @@ import spacy
 
 
 def clean_doc(document: str):
-    # if you have more computational ressources use es_core_news_lg or es_dep_news_trf
-    nlp = spacy.load("es_core_news_sm")
     """
     @param<doc>: it is a document string
     this functions its called to clean the chunkings applying
@@ -13,13 +11,17 @@ def clean_doc(document: str):
     3. removing stopwords
     5. filtering if token lenght is higher than 2 characteres
     """
+    # if you have more computational ressources use es_core_news_lg or es_dep_news_trf
+    nlp = spacy.load("es_core_news_sm")
+    spacy_stopwords = spacy.lang.es.stop_words.STOP_WORDS
     # tokenization of document
     doc = nlp.tokenizer(document)
     # cleaning text
     doc = (
         token.text.lower()
         for token in doc
-        if not token.is_stop and token.is_alpha and len(token.text) > 2
+        if token.is_alpha and len(token.text) > 2
+        # if not spacy_stopwords and token.is_alpha and len(token.text) > 2
     )
     # join the tokens into one string
     doc = " ".join(doc)
