714437035dca7c5becabb05a1ce7b8b335306d83
alcalderagl <145729419+alcalderagl@users.noreply.github.com>
2024-11-16 00:15:40 -0600

    :feat:(streamlit-app): first version

--- main_app.py
+++ main_app.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
-st.title("Hello")
+pages = {
+    "Load files and process data": [
+        st.Page("src/components/process_data_view.py", title="Load and process data"),
+        # st.Page("src/components/query_data_view.py", title="Embedding query")
+    ],
+    "Chat": [st.Page("src/components/chat_view.py", title="RAG App")],
+}
+
+pg = st.navigation(pages)
+pg.run()
