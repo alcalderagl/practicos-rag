df9791800cc63774d41ff2b43fa9f3c6ec4dd683
alcalderagl <145729419+alcalderagl@users.noreply.github.com>
2024-11-16 17:42:03 -0600

    :feat:(streamlit): show clean process

--- main_app.py
+++ main_app.py
@@ -2,8 +2,11 @@ import streamlit as st
 
 pages = {
     "Load files and process data": [
-        st.Page("src/components/process_data_view.py", title="Load and process data"),
-        # st.Page("src/components/query_data_view.py", title="Embedding query")
+        st.Page(
+            "src/components/loader/process_data/process_data_view.py",
+            title="Load and process data",
+        ),
+        st.Page("src/components/loader/directory/directory_view.py", title="Directory"),
     ],
     "Chat": [st.Page("src/components/chat_view.py", title="RAG App")],
 }
--- src/commons/logging_messages.py
+++ src/commons/logging_messages.py
@@ -3,4 +3,7 @@ LOGG_MESSAGES = {
     "LOADER_DOCUMENT_ADDED": 'The document "{filename}" was sucessfully added.',
     "LOADER_ERROR_LOADING": "Error while loading the document {error}.",
     "LOADER_UNKNOWN_FILE": 'The document "{filename}" with extension {ext} is not supported.',
+    "APP_LABEL_CLEANING_FILE": "Cleaning file - {filename}",
+    "APP_LABEL_CHUNCKING_FILE": "Chuncking file - {filename}",
+    "CHUNCKING_N_PROCESS": "Processing page {page}",
 }
