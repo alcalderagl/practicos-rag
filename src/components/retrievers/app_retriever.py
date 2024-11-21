9acb6c2dc9bf66f9c1f97f07fbbfdc3db3037023
alcalderagl <145729419+alcalderagl@users.noreply.github.com>
2024-11-21 12:05:24 -0600

    :feat:(retriever): chat retriever

--- src/components/retrievers/app_retriever.py
+++ src/components/retrievers/app_retriever.py
@@ -1,3 +1,59 @@
 import streamlit as st
+from src.commons.models.chat_retriever.chat_history import ChatHistory
+from src.retrievers.retrievers_logic import vector_retriever
+import time
 
 st.title("Query Retriever")
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+    
+user_prompt = st.chat_input("Say something")
+
+if user_prompt:
+    # prompt user
+    st.session_state.chat_history.append(ChatHistory(role="user", message=user_prompt))
+    st.session_state.chat_history.append(ChatHistory(role="bot", message="..."))
+
+st.markdown(
+    """
+    <style>
+    .user-message {
+        color: black;
+        background-color: #dcf8c6;
+        border-radius: 10px;
+        padding: 8px 12px;
+        margin-bottom: 10px;
+        width: fit-content;
+        align-self: flex-end;
+    }
+    .bot-message {
+        color: black;
+        background-color: #f1f0f0;
+        border-radius: 10px;
+        padding: 8px 12px;
+        margin-bottom: 10px;
+        width: fit-content;
+        align-self: flex-start;
+    }
+    .chat-container {
+        display: flex;
+        flex-direction: column;
+    }
+    </style>
+    """
+    ,
+    unsafe_allow_html=True
+)
+    
+if st.session_state.chat_history:
+    for i, chat in enumerate(st.session_state.chat_history):
+        if chat.role == 'user':
+            st.markdown(f'<div class="chat-container"><div class="user-message">{chat.message}</div></div>', unsafe_allow_html=True)
+        else:
+            if i == len(st.session_state.chat_history) - 1:
+                with st.spinner("..."):
+                    time.sleep(5)
+                    bot_response = "Te atiendo cuando pueda" #vector_retriever(user_prompt))
+                    chat.message = bot_response
+            st.markdown(f'<div class="chat-container"><div class="bot-message">{chat.message}</div></div>', unsafe_allow_html=True)
