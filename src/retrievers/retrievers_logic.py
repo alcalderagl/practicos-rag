385177768196e4f069dd3358809b076f7d0ac248
alcalderagl <145729419+alcalderagl@users.noreply.github.com>
2024-11-20 23:32:15 -0600

    :feat:(retriever): logic retriever

--- src/retrievers/retrievers_logic.py
+++ src/retrievers/retrievers_logic.py
@@ -2,6 +2,9 @@ import os
 from src.vector_store_client.vector_store_client_logic import VectorStoreManager
 
 
-def vector_retriever():
-    # get collection name
-    vectorStore = VectorStoreManager()
+def vector_retriever(question: str, search_type: str = "mmr", k_search: int = 10):
+    vectorStoreManager = VectorStoreManager()
+    retriever = vectorStoreManager.vector_store.as_retriever(
+        search_type=search_type, search_kwargs={"k": k_search}
+    )
+    return retriever.invoke(question)
