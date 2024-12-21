3d27990605f71cb9d1707dfff683c23bbb8b03dc
alcalderagl <145729419+alcalderagl@users.noreply.github.com>
2024-12-20 20:44:01 -0600

    :feat:(query_rewriting)

--- src/query_rewriting/query_rewriting_logic.py
+++ src/query_rewriting/query_rewriting_logic.py
@@ -1,8 +1,9 @@
 import logging
 from langchain.prompts import ChatPromptTemplate
-from langchain.chains.llm import LLMChain 
+from langchain.chains.llm import LLMChain
 from src.llm.llm_logic import LageLangueModel
 from src.query_rewriting.models.query_rewriting import QuestionRewriting
+
 logging.basicConfig(level=logging.INFO)
 
 
@@ -10,7 +11,7 @@ class QueryRewriting:
     def __init__(self):
         pass
 
-    def rewriting(self, question: str):
+    def rewriting(self, question: str) -> QuestionRewriting:
         """
         Refines a given query for better search results.
 
@@ -25,9 +26,9 @@ class QueryRewriting:
                 An object containing the original query and the rewritten query.
         """
         try:
-            
+
             LLM_model = LageLangueModel()
-            
+
             template = """Provide a better search query for \
             web search engine to answer the given question, end \
             the queries with ’**’. Remember to respond in Spanish . Question: \
@@ -37,21 +38,26 @@ class QueryRewriting:
             # OLLAMA
             # ollama = LLM.connect_to_ollama()
             # query_rewriting_chain = LLMChain(llm=ollama, prompt=query_rewriting_prompt)
-            
+
             # CHATGPT
             open_ai_llm = LLM_model.connect_chat_openAI()
-            query_rewriting_chain = LLMChain(llm=open_ai_llm, prompt=query_rewriting_prompt)
+            query_rewriting_chain = LLMChain(
+                llm=open_ai_llm, prompt=query_rewriting_prompt
+            )
             rewriting_query = query_rewriting_chain.invoke(question)
-            
+
             logging.info(f"response rewriting query: {rewriting_query}")
-            re_query:str = rewriting_query["text"]
-            re_query = re_query.replace("Consulta: \"", "").replace("Consulta mejorada: \"", "").replace("Consulta", "").replace("**\"", "").replace("**", "").strip()
+            re_query: str = rewriting_query["text"]
+            re_query = (
+                re_query.replace('Consulta: "', "")
+                .replace('Consulta mejorada: "', "")
+                .replace("Consulta", "")
+                .replace('**"', "")
+                .replace("**", "")
+                .strip()
+            )
             response = QuestionRewriting(query=question, rewriting_query=re_query)
-    
-            # Requisitos para etiquetar productos con cafeína en alimentos y bebidas?
             return response
         except (ValueError, KeyError) as e:
             logging.info(f"error when rewriting_query: {e}")
             return QuestionRewriting(query="", rewriting_query="")
-            
-        
